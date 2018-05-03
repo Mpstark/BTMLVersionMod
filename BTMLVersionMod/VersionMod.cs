b38f876f56a8bdd4da4b97f1aff925f41f62c65e
Michael P. Starkweather <mpstark@gmail.com>
2018-05-03 17:03:54 -0500

    Add project files.

---
+++ BTMLVersionMod/VersionMod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using BattleTech;
+using System.Reflection;
+
+namespace BTMLVersionMod
+{
+    [HarmonyPatch(typeof(VersionInfo), "GetReleaseVersion")]
+    public static class VersionInfo_GetReleaseVersion_Patch
+    {
+        static void Postfix(ref string __result)
+        {
+            string old = __result;
+            __result = old + " w/ BTML";
+        }
+    }
+
+    public static class VersionMod
+    {
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("io.github.mpstark.BTMLVersionMod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
